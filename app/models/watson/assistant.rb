4fc9972975160835769b68f4e76a13250a963299
Manuel Ortega <manuel@lasindias.coop>
2018-09-16 11:37:13 -0500

    Updated watson bot

--- app/models/watson/assistant.rb
+++ app/models/watson/assistant.rb
@@ -6,20 +6,7 @@ class Watson::Assistant
   VERSION = "alpha"
   NAME = "TestFAQ"
   LAMBDA_FUNCTION_NAME = "TestFAQFunction"
-=begin
-  CLIENT = Aws::LexModelBuildingService::Client.new(region: ENV["LEX_FAQ_AWS_REGION"],
-    access_key_id: ENV["LEX_FAQ_AWS_ACCESS_ID"],
-    secret_access_key: ENV["LEX_FAQ_AWS_SECRET_ACCESS_KEY"])
-=end
 
   def self.current
-=beging
-    data = CLIENT.get_bot({
-      name: Lex::Bot::NAME,
-      version_or_alias: "$LATEST",
-    })
-
-    self.new(name: Lex::Bot::NAME, data: data)
-=end
   end
 end
