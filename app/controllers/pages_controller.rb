ea30818c5facfbc80d214876542815a15f7f01ec
Mayra Rodriguez <mers85@gmail.com>
2018-10-13 15:33:32 -0400

    added card project

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
 
   def home
+    @response = Project.index(session, {})
+    @projects = @response.parsed_body
+    @projects = @projects.take(3)
+
   end
-  
+
 end
