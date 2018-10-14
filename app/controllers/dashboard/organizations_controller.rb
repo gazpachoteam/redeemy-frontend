39fa78eff866fa520eab3b57b51e3e275882101a
Manuel Ortega <manuel@lasindias.coop>
2018-10-14 09:27:58 -0500

    First version of dashboard

--- app/controllers/dashboard/organizations_controller.rb
+++
@@ -1,7 +0,0 @@
-class Dashboard::OrganizationsController < DashboardController
-  def index
-    @response = Organization.index(session, {}, params[:provider_id])
-    @organizations = @response.parsed_body
-
-  end
-end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,3 +1,12 @@
 class DashboardController < ApplicationController
-  layout 'dashboard'
+
+  def index
+    if has_role? "OrgAdmin"
+      @response = Organization.org_admin_index(session, {}, session[:user_id])
+      @organization = @response.parsed_body
+    elsif has_role? "Admin"
+      @response = Organization.index(session, {})
+      @organizations = @response.parsed_body
+    end
+  end
 end
